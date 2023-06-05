3088efc406e51e966ecb598a794cd27eb141572f
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2023-06-05 08:43:48 +0200

    Initial commit

---
+++ src/js/swiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2.5,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
